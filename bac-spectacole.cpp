14e77868bc00110b79de1cfab037197493d036a5
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2022-12-24 19:05:05 +0200

    Added bac-spectacole.cpp

---
+++ bac-spectacole.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//
+//struct sala
+//{
+//	int nrLocuri;
+//	int pret;
+//} t[15];
+//
+//int main()
+//{
+//	int s = 0;
+//
+//	for (int i = 0; i < 15; ++i)
+//		s += t[i].nrLocuri * t[i].pret;
+//}
