b2bc227d69ad17a52a34f7d28d86901a1c8650fa
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2022-12-24 18:57:16 +0200

    Added FConcat.cpp

---
+++ FConcat.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <cstring>
+//
+//void FConcat(char s[], char d[])
+//{
+//	int i = 0, j = 0;
+//
+//	for (i = 0; s[i] != '\0'; i++)
+//	{
+//
+//	}
+//
+//	for (j = 0; d[j] != '\0'; j++, i++)
+//	{
+//		s[i] = d[j];
+//	}
+//
+//	s[i] = '\0';
+//}
+//
+//int main()
+//{
+//	char s[] = "abc", d[] = "def";
+//
+//	FConcat(s, d);
+//
+//	std::cout << s;
+//
+//}
