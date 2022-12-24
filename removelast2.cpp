9202bf9ff4ad747d1180ad714e9c47308fd3b98b
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2022-12-24 19:07:17 +0200

    Added removelast2.cpp

---
+++ removelast2.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//
+//int n;
+//
+//int main()
+//{
+//	std::cin >> n;
+//
+//	n = n & ~(1u);
+//
+//	n = n | (0);
+//
+//	n = n & ~(1u << 1);
+//
+//	n = n | (0u << 1);
+//
+//	std::cout << n;
+//}
