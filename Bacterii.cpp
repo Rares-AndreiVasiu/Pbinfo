49ceb6c876b5c4e2a888577c0296ac4827a68fe9
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2022-12-24 18:54:22 +0200

    Added Bacterii.cpp

---
+++ Bacterii.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <cmath>
+//#include <climits>
+//
+//long long n, m, mini, neinfectate, total, infectate;
+//
+//int main()
+//{
+//	std::cin >> n >> m;
+//
+//	mini = std::min(n, m);
+//
+//	total = n * m;
+//
+//	infectate = mini * (mini + 1) / 2;
+//
+//	neinfectate = total - infectate;
+//
+//	std::cout << neinfectate;
+//
+//	return 0;
+//}
