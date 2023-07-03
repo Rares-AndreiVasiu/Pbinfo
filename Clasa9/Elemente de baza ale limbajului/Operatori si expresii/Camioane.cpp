937a2be1e496fd200982588199621670e5ccf627
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2023-07-03 13:26:16 +0300

    100p Camioane

---
+++ Clasa9/Elemente de baza ale limbajului/Operatori si expresii/Camioane.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t1, t2, m, n, z;
+
+    cin >> t1 >> t2 >> n >> m >> z;
+
+    cout << (n * t1 + m * t2) * z << endl;
+
+    return 0;
+}
