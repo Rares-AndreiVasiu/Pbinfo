689cf5c65b29ea7a531cc7305f16638f0d6a1ee4
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2023-07-03 14:00:55 +0300

    100p primacifraapartiizecimale

---
+++ Clasa9/Elemente de baza ale limbajului/Operatori si expresii/primacifraapartiizecimale.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double n;
+
+    int a, a1;
+
+    cin >> n;
+
+    a = n * 10;
+
+    a1 = int(a);
+
+    cout << a1 % 10;
+
+    return 0;
+}
