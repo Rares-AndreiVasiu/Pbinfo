e44b1e85f953f49aaa213534d53ef5d1220121e4
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2023-07-03 13:38:39 +0300

    100p picioare

---
+++ Clasa9/Elemente de baza ale limbajului/Operatori si expresii/picioare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int G, O;
+    
+    cin >> G >> O;
+    
+    cout << G + O << " " << 2 * G + 4 * O << endl;
+    
+    return 0;
+}
