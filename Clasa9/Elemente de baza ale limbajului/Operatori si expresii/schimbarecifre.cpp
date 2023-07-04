59963f4764e37e995629355f6f480999f5d828c9
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2023-07-04 20:59:11 +0300

    100p schimbarecifre

---
+++ Clasa9/Elemente de baza ale limbajului/Operatori si expresii/schimbarecifre.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n, z, u, a, n1;
+    cin >> n;
+    if (n % 10 == 0)
+    {
+        z = (n / 10) % 10;
+        a = pow(z, 2);
+        cout << a;
+    }
+    else
+    {
+        u = n % 10;
+        z = (n / 10) % 10;
+        a = u * 10 + z;
+        n1 = pow(a, 2);
+        cout << n1;
+    }
+    return 0;
+}
