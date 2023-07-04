e869062b3e711a8774734ec2406ea9152d45fa8b
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2023-07-04 20:40:42 +0300

    100p sumapatratecifre

---
+++ Clasa9/Elemente de baza ale limbajului/Operatori si expresii/sumapatratecifre.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    cout << (a % 10) * (a % 10) + (a / 10 % 10) * (a / 10 % 10) + 
+    (a / 100) * (a / 100);
+
+    return 0;
+}
